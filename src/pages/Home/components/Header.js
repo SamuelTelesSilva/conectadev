2bf6e568d49c276d26aef8a7d2029c0a2842c939
SamuelTelesSilva <samuel.silva@uni9.edu.br>
2020-07-31 16:12:22 -0300

    Add appBar style

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -1,24 +1,69 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import {Button} from '@material-ui/core';
-import  '../style.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import {Bell} from 'react-feather';
+import Avatar from '@material-ui/core/Avatar';
+
+
+const useStyles = makeStyles({
+    appBar: {
+        boxShadow: 'none'
+    },
+    img: {
+        maxHeight: 55,
+    },
+    grow: {
+        flexGrow: 1
+    },
+    userSection: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    button: {
+        marginRight: 10
+    },
+    bell: {
+        marginRight: 10
+    }
+
+
+})
 
 function Header () {
+    const classes = useStyles();
     return(
-        <header className="header">
-        <div className="toolbar">
-            <div>
-                <a href="/">Conect Dev</a>
-                <input type="text"></input>
-            </div>
-            <div>
-                <Button variant="contained" color="primary">
-                    Novo Post
-                </Button>
-                <span>img1</span>
-                <span>img2</span>
-            </div>
-        </div>
-    </header>
+        <AppBar position="fixed" color="inherit" className={classes.appBar}>
+            <Toolbar>
+                <img src="/images/logo.png" alt="logo" className={classes.img}/>
+                <div className={classes.grow}></div>
+
+                <div className={classes.userSection}>
+                    <Button variant="contained" color="primary" className={classes.button}> 
+                        Novo Post
+                    </Button>
+                    <SvgIcon className={classes.bell}>
+                        <Bell></Bell>
+                    </SvgIcon>
+                    <Avatar alt="Remy Sharp" src="" />
+                </div>
+           { /*
+                <div>
+                    <a href="/">Conect Dev</a>
+                    <input type="text"></input>
+                </div>
+                <div>
+                    <Button variant="contained" color="primary">
+                        Novo Post
+                    </Button>
+                    <span>img1</span>
+                    <span>img2</span>
+                </div>
+                */}
+            </Toolbar>
+        </AppBar>
     )
 }
 export default Header;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
-import './style.css';
-import Header from './components/Header'
+import { makeStyles } from '@material-ui/core/styles';
 
+import Header from './components/Header'
+import NavBar from './components/NavBar'
+import Feed from './components/Feed'
+
+//usando css do materialUI dentro do arquivo js
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+
+    main: {
+        border: '1px solid orange',
+        height: '100vh',
+        display: 'flex',
+        width: '1200px',
+        margin: '0 auto',
+    }
+})
 
 function Home () {
+    const classes = useStyles(); //chamando o useStyles
+
     return(
-        <div>
+        <div className={classes.root}>
            <Header />
             <div className="toolbar"></div>
-            <main className="main">
-                <div className="navbar">
-                    navbar
-                </div>
-
-                <div className="feed">
-                    feed
-                </div>
-                
+            <main className={classes.main}>
+                <NavBar />
+                <Feed />                
             </main>
         </div>
     )
